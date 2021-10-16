40aa306ff5d3733f15cc6080f34fc544367fad31
むずでょ <muzudho1@gmail.com>
2021-10-16 23:31:30 +0900

    coord

--- README.md
+++ README.md
@@ -13,3 +13,6 @@ main.py
 ## Documents
 
 📖 [PythonのTkinterを使ってみる](https://qiita.com/nnahito/items/ad1428a30738b3d93762)  
+📖 [【Python】テキストボックス(Tkinter.Entry)の作成・出力・入力](https://pg-chain.com/python-tkinter-entry)  
+📖 [Tkinter、Textウィジェットの使い方](https://blog.narito.ninja/detail/100/)  
+
--- main.py
+++ main.py
@@ -1,8 +1,33 @@
 import sys
-import tkinter
+import tkinter as tk
 
-root = tkinter.Tk()
-root.title(u"Tkinter practice")
-root.geometry("400x300")
+window = tk.Tk()
+window.title(u"Tkinter practice")
+window.geometry("480x360")
 
-root.mainloop()
+"""
+                 2  2     2  2         4  4
+      1          1  2     6  7         7  8
+   0  0          0  0     0  0         0  0
+  0+--------------------------------------+
+   |                                      |
+ 10|  +----------+           +---------+  |
+   |  | Japanese |           | English |  |
+ 40|  +----------+           +---------+  |
+ 50|  +----------+  +-----+  +---------+  |
+   |  |          |  | --> |  |         |  |
+   |  |          |  +-----+  |         |  |
+   |  |          |  +-----+  |         |  |
+   |  |          |  | <-- |  |         |  |
+350|  +----------+  +-----+  +---------+  |
+   |                                      |
+360+--------------------------------------+
+"""
+
+# 左テキストエリア
+left_text_area = tk.Text(window).place(x=10, y=50, width=200, height=300)
+
+# 右テキストエリア
+left_text_area = tk.Text(window).place(x=270, y=50, width=200, height=300)
+
+window.mainloop()
